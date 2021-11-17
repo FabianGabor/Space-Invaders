1ef214b9f60ad50fe1ce6a0f501f363f6e4cea9d
Fábián Gábor <info@fabiangabor.com>
2021-11-18 00:35:58 +0100

    Aliens class

--- Space Invaders/Aliens.cs
+++ Space Invaders/Aliens.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,44 @@ namespace Space_Invaders
     internal class Aliens
     {
         public const int COLUMNS = 11;
-        public const int ROWS = 5;
+        public const int ROWS = 1;
         public const int ALIENCOUNT = ROWS * COLUMNS;
         public const int ALIENSPEED = 5;
         public const int ALIENBULLETTIMER = 300;
         public const int ALIENMARGIN = 30;        
         public const int ALIENGROUPWIDTH = 960;
         
-
-        private int alienSpeed = ALIENSPEED;
-        private int alienBulletTimer = ALIENBULLETTIMER;
-
         public PictureBox[,] grid;
+        //public Alien[,] alien;
 
         public Aliens()
         {
+            grid = new PictureBox[ROWS, COLUMNS];            
         }
+
+        public void CreateAliens(Panel panelGameBoard)
+        {
+            int top = 0;
+            for (int i = 0; i < ROWS; i++)
+            {
+                int left = (panelGameBoard.Width - Aliens.ALIENGROUPWIDTH) / 2;
+
+                for (int j = 0; j < COLUMNS; j++)
+                {                    
+                    grid[i, j] = new PictureBox
+                    {
+                        Size = new Size(60, 45),
+                        Margin = new Padding(Aliens.ALIENMARGIN),
+                        Image = Properties.Resources.alien,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Top = top,
+                        Left = left,
+                        Tag = new Property(Tags.alien, i, j)
+                };                                       
+                    left += grid[i, j].Size.Width + ALIENMARGIN;                    
+                }
+                top += grid[i, 0].Size.Height + ALIENMARGIN;                
+            }
+        }        
     }
 }
