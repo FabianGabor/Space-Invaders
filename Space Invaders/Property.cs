0e2b97d3c681108bbd020139b216077451c16f85
Fábián Gábor <info@fabiangabor.com>
2021-11-18 00:36:24 +0100

    Property class

---
+++ Space Invaders/Property.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    internal class Property
+    {
+        public Tags tag;
+        public int row;
+        public int col;
+
+        public Property(Tags tag, int row, int col)
+        {
+            this.tag = tag;
+            this.row = row;
+            this.col = col;
+        }
+    }
+}
