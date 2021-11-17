043ad54db65a82c6d5cefcd3d73901080598da03
Fábián Gábor <info@fabiangabor.com>
2021-11-17 20:35:57 +0100

    Aliens class

---
+++ Space Invaders/Aliens.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    internal class Aliens
+    {
+        public const int COLUMNS = 11;
+        public const int ROWS = 5;
+        public const int ALIENCOUNT = ROWS * COLUMNS;
+        public const int ALIENSPEED = 5;
+        public const int ALIENBULLETTIMER = 300;
+        public const int ALIENMARGIN = 30;        
+        public const int ALIENGROUPWIDTH = 960;
+        
+
+        private int alienSpeed = ALIENSPEED;
+        private int alienBulletTimer = ALIENBULLETTIMER;
+
+        public PictureBox[,] grid;
+
+        public Aliens()
+        {
+        }
+    }
+}
