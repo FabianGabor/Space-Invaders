19273cb1c7c5650d74b6f7594a8615402a23d861
Fábián Gábor <info@fabiangabor.com>
2021-11-18 00:35:48 +0100

    Alien class

---
+++ Space Invaders/Alien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    internal class Alien
+    {
+        public PictureBox pictureBox;
+        public int row;
+        public int col;
+
+        public Alien()
+        {
+        }
+    }
+}
